9a0dd64a0bd4cbf69c49452bd7691203f12583dc
rezi93 <103065023+rezi93@users.noreply.github.com>
2022-11-28 22:38:51 +0400

    add js

---
+++ script.js
@@ -0,0 +1,83 @@
+// let user={
+//    username:'giorgi',
+//    lastname:"smith",
+//    age:25,
+//    studentstatus:"active"
+// };
+// console.log(user.studentstatus);
+
+
+
+
+
+
+// let numbers=[10,5,20,15,19];
+// for(let n of numbers){
+//     if(n>5){
+//         console.log(n);
+//     }
+// }
+
+
+
+
+
+
+// let numbers=[5,6,10,25,3,7,4,100,28,36,89]
+// for(let y of numbers)
+// if (y>5){
+//     console.log(y);
+// }
+
+
+
+
+
+
+// let user={
+//     name:'giorgi',
+//     age:20,
+//     studentstatus:'active'
+
+
+// }
+
+
+    //  if((user.age<18) && (user.studentstatus==true))
+    //  {
+    //     console.log('hello');}
+
+    // else {console.log('error');}
+
+
+        // if (user.name=='levani'){
+        //     console.log('hello levani');}
+
+        //     else {console.log('error');}
+        
+        
+        // if ((user.userstatus=='active') || (user.age<25)){
+        //     console.log('hello world');
+        // }
+        // else {console.log('error');}
+
+
+
+
+
+
+//         let array=['watermelon','pear',10,45,50,'apple','ananas']
+//         for(let x of array ){
+//    X         if(x==='string')
+//                 console.log(x);
+//             }
+
+
+
+
+    
+
+
+
+
+
